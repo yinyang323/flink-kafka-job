6c6daee802c3b21ad48eb2f0e4dd1040e76ea0e0
18179130592@163.com <18179130592@163.com>
2019-11-01 11:13:24 +0800

    Changed to restful

---
+++ src/main/java/com/myflink/common/OkHttpHelper.java
@@ -0,0 +1,127 @@
+package com.myflink.common;
+
+import com.alibaba.fastjson.JSON;
+import com.myflink.messages.record;
+import okhttp3.*;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.io.IOException;
+
+public class OkHttpHelper {
+    static private OkHttpClient _client=new OkHttpClient();
+
+    public static String getResult() {
+        return result;
+    }
+
+    static private String result;
+
+    static public void createInstancce(String _groupid1,String url_create){
+
+        RequestBody body = new FormBody.Builder()
+                .add("groupid", _groupid1)
+                .add("num", "1")
+                .build();
+
+        Request request1 = new Request.Builder()
+                .url(url_create)
+                .post(body)
+                .build();
+
+        _client.newCall(request1).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(call.toString());
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    System.out.println(response.body().string());
+                }
+                response.close();
+            }
+        });
+
+    }
+
+    static public void Comsume(String _url, SourceFunction.SourceContext sc){
+
+        Request request = new Request.Builder()
+                .url(_url)
+                .get()
+                .build();
+    try {
+        _client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(call.toString());
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String _result = response.body().string();
+                    _result = _result.replace("[\"", "");
+                    _result = _result.replace("\"]", "");
+                    _result = _result.replace("\\", "");
+                    if (_result.contains(">")) {
+                        // this synchronized block ensures that state checkpointing,
+                        // internal state updates and emission of elements are an atomic operation
+                        //synchronized (sourceContext.getCheckpointLock()) {
+                        //sourceContext.collect(result);
+                        // }
+                        sc.collect(_result);
+                    } else {
+                        //sourceContext.markAsTemporarilyIdle();
+                        System.out.println("Illegal result: " + _result);
+                    }
+                }
+                response.close();
+            }
+        });
+    }
+    catch (Exception ex){
+        System.out.println(ex.toString());
+    }
+
+    }
+
+    public static void product(String _url,String value){
+        RequestBody body=RequestBody
+                .create(MediaType.parse("application/json;charset=utf-8"),JSON.toJSON(toObj(value)).toString());
+        Request request = new Request.Builder()
+                .url(_url)
+                .post(body)
+                .build();
+
+        _client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("fail: "+call.toString());
+                System.out.println(e.getMessage());
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    System.out.println(response.body().string());
+                }
+                response.close();
+            }
+        });
+    }
+
+    private static record toObj(String value){
+        record _record=new record();
+        _record.setKey("ICE processed");
+        _record.setValue(value);
+        _record.setPartition(0);
+
+        return _record;
+    }
+
+
+
+}
