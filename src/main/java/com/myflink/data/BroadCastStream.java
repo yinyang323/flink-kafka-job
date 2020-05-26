8099f26c6bc2e4e8b751bc4c21c649697d85afff
yinyang323 <18179130592@163.com>
2020-05-27 00:58:54 +0800

    table process

--- src/main/java/com/myflink/data/BroadCastStream.java
+++ src/main/java/com/myflink/data/BroadCastStream.java
@@ -1,39 +1,54 @@
 package com.myflink.data;
 
-import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 
-import org.apache.flink.calcite.shaded.org.apache.commons.codec.language.bm.Rule;
-import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
-import org.apache.flink.util.Collector;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
-public class BroadCastStream extends BroadcastProcessFunction<String, Rule, String> {
+import java.time.LocalDateTime;
+import java.util.Set;
 
-    // store partial matches, i.e. first elements of the pair waiting for their second element
-    // we keep a list as we may have many first elements waiting
-    private final MapStateDescriptor<String, String> mapStateDesc =
-            new MapStateDescriptor<>(
-                    "items",
-                    BasicTypeInfo.STRING_TYPE_INFO,
-                    BasicTypeInfo.STRING_TYPE_INFO);
+public class BroadCastStream extends RichParallelSourceFunction<String> {
 
-    // identical to our ruleStateDescriptor above
-    private final MapStateDescriptor<String, Rule> ruleStateDescriptor =
-            new MapStateDescriptor<>(
-                    "RulesBroadcastState",
-                    BasicTypeInfo.STRING_TYPE_INFO,
-                    TypeInformation.of(new TypeHint<Rule>() {}));
-
+    private volatile boolean isRun;
+    private volatile int lastUpdateMin = -1;
 
     @Override
-    public void processElement(String value, ReadOnlyContext readOnlyContext, Collector<String> collector) throws Exception {
-
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        isRun = true;
     }
 
     @Override
-    public void processBroadcastElement(Rule value, Context ctx, Collector<String> collector) throws Exception {
-        ctx.getBroadcastState(ruleStateDescriptor).put(value.getPattern(), value);
+    public void run(SourceContext<String> ctx) throws Exception {
+        while (isRun) {
+            LocalDateTime date = LocalDateTime.now();
+            int min = date.getMinute();
+            if (min != lastUpdateMin) {
+                lastUpdateMin = min;
+                Set<String> configs = readConfigs();
+                if (configs != null && configs.size() > 0) {
+                    for (String config : configs) {
+                        ctx.collect(config);
+                    }
+
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    private Set<String> readConfigs() {
+        //这里读取配置信息
+        Config config = ConfigService.getAppConfig();
+
+        return null;
+    }
+
+
+    @Override
+    public void cancel() {
+        isRun = false;
     }
 }
